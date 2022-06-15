cea1aa248b64d36d07de672d154c200ac803c37d
Wok <woctezuma@users.noreply.github.com>
2022-06-15 12:55:15 +0200

    Add main script for ASF IPC

---
+++ run_ASF_IPC.py
@@ -0,0 +1,12 @@
+from src.demo_utils import play_demos
+
+
+def main():
+    app_ids = [1884590, 1905170]  # "Tinykin Demo" and "Hell Pie Demo"
+    play_demos(app_ids)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
