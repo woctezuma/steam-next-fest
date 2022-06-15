e37657351d8c731538803ea698498ca487a4ccc5
Wok <woctezuma@users.noreply.github.com>
2022-06-15 12:59:43 +0200

    Add main script for SteamWebPipes

---
+++ run_SteamWebPipes.py
@@ -0,0 +1,19 @@
+import asyncio
+import websockets
+
+
+async def handler(websocket):
+    while True:
+        message = await websocket.recv()
+        print(message)
+
+
+async def main():
+    url = "ws://localhost:8181"
+    async with websockets.connect(url) as ws:
+        await handler(ws)
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
